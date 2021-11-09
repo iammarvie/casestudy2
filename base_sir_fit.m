654a3e50a850e26b1d34b4ff7f1ec7f22e97dbd2
Prince John <prince.apple.john@gmail.com>
2021-11-09 11:32:34 -0600

    Added graphs for all SIRD models

--- base_sir_fit.m
+++ base_sir_fit.m
@@ -8,23 +8,39 @@
 stl = COVID_MO(string(COVID_MO.name) == 'St. Louis', :);
 springfield = COVID_MO(string(COVID_MO.name) == 'Springfield', :);
 jefferon = COVID_MO(string(COVID_MO.name) == 'Jefferson City', :);
+%% Dates of interest for STL
+stl_period1 = stl(isbetween(stl.date,datetime('2020-07-01'),datetime('2020-10-31')), :);
+stl_period2 = stl(isbetween(stl.date,datetime('2020-10-31'),datetime('2021-02-03')), :);
+stl_period3 = stl(isbetween(stl.date,datetime('2021-07-14'),datetime('2021-10-06')), :);
+
+stl_period1_array = double(table2array(period1(:,[3:4])))./2805473;
+stl_period2_array = double(table2array(period2(:,[3:4])))./2805473;
+stl_period3_array = double(table2array(period3(:,[3:4])))./2805473;
+%% Dates of interest for springfield
+spring_period1 = springfield(isbetween(springfield.date,datetime('2020-11-02'),datetime('2021-01-31')), :);
+spring_period2 = springfield(isbetween(springfield.date,datetime('2021-06-08'),datetime('2021-09-15')), :);
+
+spring_period1_array = double(table2array(spring_period1(:,[3:4])))./475220;
+spring_period2_array = double(table2array(spring_period2(:,[3:4])))./475220;
+%% Dates of interest for Jefferson
+jeff_period1 = jefferon(isbetween(jefferon.date,datetime('2020-11-02'),datetime('2021-01-31')), :);
+jeff_period2 = jefferon(isbetween(jefferon.date,datetime('2021-06-08'),datetime('2021-09-15')), :);
+
+jeff_period1_array = double(table2array(jeff_period1(:,[3:4])))./150198;
+jeff_period2_array = double(table2array(jeff_period2(:,[3:4])))./150198;
+
+%% Data for the entire range
+stl_all = double(table2array(stl(:,[3:4])))./2805473;
+springfield_all = double(table2array(springfield(:,[3:4])))./475220;
+jefferson_all = double(table2array(jefferon(:,[3:4])))./150198;
+
 %%
-coviddata = double(table2array(stl(:,[3:4])))./2805473;
-t = height(stl);
-dates =  table2array(stl(:,1)); 
 
-springfield_all = 
+coviddata = spring_period2_array;
+period = spring_period2;
+t = height(coviddata);
+dates =  table2array(period(:,1)); 
 
-
-
-% The following line creates an 'anonymous' function that will return the cost (i.e., the model fitting error) given a set
-% of parameters.  There are some technical reasons for setting this up in this way.
-% Feel free to peruse the MATLAB help at
-% https://www.mathworks.com/help/optim/ug/fmincon.html
-% and see the sectiono on 'passing extra arguments'
-% Basically, 'sirafun' is being set as the function siroutput (which you
-% will be designing) but with t and coviddata specified.
-%%
 sirafun= @(x)siroutput(x,t,coviddata);
 
 %% set up rate and initial condition constraints
@@ -39,7 +55,7 @@ b = [];
 % Hint: For example, the sum of the initial conditions should be
 % constrained
 % If you don't want such a constraint, keep these matrices empty.
-Af = [0 1 1 1 1 0 0 0 0; 0 0 0 0 0 1 1 1 1];
+Af = [1 1 1 1 1 0 0 0 0; 0 0 0 0 0 1 1 1 1];
 bf = [1; 1];
 
 %% set up upper and lower bound constraints
@@ -47,7 +63,7 @@ bf = [1; 1];
 % lb < x < ub
 % here, the inequality is imposed element-wise
 % If you don't want such a constraint, keep these matrices empty.
-ub = [0.30, 1, 0.1, 1, 0.5, 1, 0.7, 1, 0.1]';
+ub = [0.30, .995, 0.1, 1, 0.5, 1, 0.7, 1, 0.1]';
 lb = [0, 0, 0, 0, 0, .90, 0, 0, 0]';
 
 % Specify some initial parameters for the optimizer to start from
@@ -66,7 +82,7 @@ x = fmincon(sirafun,x0,A,b,Af,bf,lb,ub);
 
 Y_fit = siroutput_full(x,t);
 
-
+%%
 % Test Plot over all st louis data
 figure();
 hold on;
@@ -77,9 +93,20 @@ hold off;
 legend('S','I','R','D','Actual Cases', 'Actual Deaths');
 xlabel('Dates')
 ylabel('Percentage Population')
-title('SIRD Fit for all STL Data')
+title("SIRD Fit for Springfield Data from " + datestr(dates(1)) + " to " + datestr(dates(length(dates))))
 
 
 % Other Plots
+%%
+figure();
+hold on;
+plot(datenum(dates),Y_fit);
+plot(datenum(dates),coviddata);
+datetick('x', 'yyyy-mm-dd','keepticks');
+hold off;
+legend('S','I','R','D','Actual Cases', 'Actual Deaths');
+xlabel('Dates')
+ylabel('Percentage Population')
+title('SIRD Fit for all Springfield Data')
 
 
